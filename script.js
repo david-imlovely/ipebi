29ac3ceffeaac9a0272e1b2bca85c41ae430c04b
David <davidtanimowo01@gmail.com>
2024-01-14 10:21:12 +0100

    initial presentation

---
+++ script.js
@@ -0,0 +1,11 @@
+// // code for changing navbar bg color
+// window.addEventListener("scroll", function () {
+//   const navbar = document.getElementById("navbar");
+//   if (window.scrollY > 50) {
+//     navbar.classList.remove("transparent");
+//     navbar.classList.add("scroll");
+//   } else {
+//     navbar.classList.remove("scroll");
+//     navbar.classList.add("transparent");
+//   }
+// });
